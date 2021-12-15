3dcd49a681c68302c9c56358ba619eccadf20d49
Jordan Lorcet <jordan.lorcet@gmail.com>
2021-12-15 13:40:41 +0100

    removed get static props

--- components/HomeMain.js
+++ components/HomeMain.js
@@ -1,21 +1,22 @@
 import HomeMainItems from "./HomeMainItems";
+import { homeMainElements } from "../libs/homeElements";
 
-function HomeMain({ homeMainElements }) {
-    return (
-        <div>
-            {homeMainElements.map(element => (
-                <HomeMainItems
-                    title={element.title}
-                    description={element.description}
-                    btn={element.btn}
-                    isMain={element.isMain}
-                    isReversed={element.isReversed}
-                    images={element.img}
-                    key={element.id}
-                />
-            ))}
-        </div>
-    )
+function HomeMain() {
+  return (
+    <div>
+      {homeMainElements.map((element) => (
+        <HomeMainItems
+          title={element.title}
+          description={element.description}
+          btn={element.btn}
+          isMain={element.isMain}
+          isReversed={element.isReversed}
+          images={element.img}
+          key={element.id}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default HomeMain;
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,8 @@ import Head from "next/head";
 import HomeMain from "../components/HomeMain";
 import HomeGallery from "../components/HomeGallery";
 import HomeFeatures from "../components/HomeFeatures";
-import { homeMainElements } from "../libs/homeElements";
 
-export const getStaticProps = async () => {
-  return {
-    props: { homeMainElements },
-  };
-};
-
-export default function Home({ homeMainElements }) {
+export default function Home() {
   return (
     <div>
       <Head>
@@ -18,7 +11,7 @@ export default function Home({ homeMainElements }) {
         <meta name="description" content="Photosnap website made with nextJs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomeMain homeMainElements={homeMainElements} />
+      <HomeMain />
       <HomeGallery />
       <HomeFeatures />
     </div>
